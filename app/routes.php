dabcb26a16dad3bc43aceb1f5ebc4a859e400fad
Dex Barrett <dexter.x740@gmail.com>
2014-03-16 16:48:16 -0700

    Add CSRF filter to login form

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,7 @@ Route::get('/', array('as' => 'index', 'uses' => 'PostController@getIndex'));
 Route::get('logout', array('as' => 'logout', 'uses' => 'UserController@getLogout'));
 Route::get('login', array('as' => 'login', 'uses' => 'UserController@getLogin'));
 
-Route::post('login', array('as' => 'login', 'uses' => 'UserController@postLogin'));
+Route::post('login', array('as' => 'login', 'uses' => 'UserController@postLogin', 'before' => 'csrf'));
 
 Route::group(array('before' => 'auth'), function(){
     Route::get('add', array('as' => 'add_new_post', 'uses' => 'PostController@getAdd'));
