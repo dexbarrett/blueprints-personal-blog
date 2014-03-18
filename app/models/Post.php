f1f692b84a6b435f3ca0f42acfdb745bda683720
Dex Barrett <dexter.x740@gmail.com>
2014-03-17 17:07:20 -0700

    Show summarize of posts in home page

--- app/models/Post.php
+++ app/models/Post.php
@@ -7,4 +7,16 @@ class Post extends Eloquent
     {
         return $this->belongsTo('User');
     }
+
+    public function summarize($length = 40)
+    {
+        $content = $this->content;
+
+        if (strlen($content) > $length) {
+            $content = substr($content, 0, $length) . ' [...]';
+        }
+
+        return $content;
+
+    }
 }
